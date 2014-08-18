ea64b8d639d048fdb6d91f73a6fb61b0ac881969
Jacob <Jacob@192.168.0.119>
2014-08-18 11:44:08 -0400

    Making this game with a Friend

---
+++ src/project/anti/utopia/Main.java
@@ -0,0 +1,18 @@
+package project.anti.utopia;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author Jacob
+ */
+public class Main extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
