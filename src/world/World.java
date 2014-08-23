0cd6f784c9e0e9213f9d9cd573a1c2097cc3ac30
Jacob <Jacob@192.168.0.118>
2014-08-23 19:11:05 -0400

    generateSquare() method bug fixed! Works perfectly. Going on to more world gen.

--- src/world/World.java
+++ src/world/World.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package world;
 
 import data.Tiles;
@@ -12,86 +11,87 @@ import data.Tiles;
  *
  * @author Jacob
  */
-
 public class World {
-    
+
     public Chunk[] chunks;
     public int length, width;
-    
-    public World(int l, int w){
+
+    public World(int l, int w) {
         chunks = new Chunk[(l / Chunk.size) * (w / Chunk.size)];
         length = l;
         width = w;
-        for(int y = 0; y < w / Chunk.size; y++){
-            for(int x = 0; x < l / Chunk.size; x ++){
+        for (int y = 0; y < w / Chunk.size; y++) {
+            for (int x = 0; x < l / Chunk.size; x++) {
                 chunks[x + y * (l / Chunk.size)] = new Chunk(x * Chunk.size, y * Chunk.size);
             }
         }
-        setBlock(Tiles.URANIUM_ORE,1,1);
-        setBlock(Tiles.URANIUM_ORE,3,1);
-        setBlock(Tiles.URANIUM_ORE,2,2);
-        setBlock(Tiles.URANIUM_ORE,0,3);
-        setBlock(Tiles.URANIUM_ORE,1,4);
-        setBlock(Tiles.URANIUM_ORE,2,4);
-        setBlock(Tiles.URANIUM_ORE,3,4);
-        setBlock(Tiles.URANIUM_ORE,4,3);
-        generateSquare(5,5,3,Tiles.AIR);
-        
+
+        generateSquare(0, 0, 5, Tiles.AIR);
+
+        setBlock(Tiles.URANIUM_ORE, 1, 1);
+        setBlock(Tiles.URANIUM_ORE, 3, 1);
+        setBlock(Tiles.URANIUM_ORE, 2, 2);
+        setBlock(Tiles.URANIUM_ORE, 0, 3);
+        setBlock(Tiles.URANIUM_ORE, 1, 4);
+        setBlock(Tiles.URANIUM_ORE, 2, 4);
+        setBlock(Tiles.URANIUM_ORE, 3, 4);
+        setBlock(Tiles.URANIUM_ORE, 4, 3);
+
     }
-    
-    public Tiles getBlock(int x, int y){
-        
+
+    public Tiles getBlock(int x, int y) {
+
         int xChunk = x / Chunk.size;
         int yChunk = y / Chunk.size;
         int chunkRows = length / Chunk.size;
-        
+
         Chunk c = chunks[xChunk + yChunk * chunkRows];
-        
+
         int xPos = x % Chunk.size;
         int yPos = y % Chunk.size;
-        
+
         int blockType = c.data[xPos + yPos * Chunk.size];
-        
+
         Tiles block = Tiles.values()[blockType];
-        
+
         return block;
-        
+
     }
-    
-    public void setBlock(Tiles block, int x, int y){
-        
+
+    public void setBlock(Tiles block, int x, int y) {
+
         int xChunk = x / Chunk.size;
         int yChunk = y / Chunk.size;
         int chunkRows = length / Chunk.size;
-        
+
         Chunk c = chunks[xChunk + yChunk * chunkRows];
-        
+
         int xPos = x % Chunk.size;
         int yPos = y % Chunk.size;
-        
+
         c.data[xPos + yPos * Chunk.size] = block.ordinal();
     }
-    
-    public void generateMineshaft(int x, int y, int l, boolean r){
-        
+
+    public void generateMineshaft(int x, int y, int l, boolean r) {
+
     }
-    
-    public void generateDragonTemple(int x, int y){
-        
+
+    public void generateDragonTemple(int x, int y) {
+
     }
-    
-    public void generateCave(int x, int y){
-        
+
+    public void generateCave(int x, int y) {
+
     }
-    
-    public void generateSquare(int x, int y, int size, Tiles tile){
-    
-        for(int rows = x; rows < x + size; x++){
-            for(int cols = y; cols < y + size; cols ++){
+
+    public void generateSquare(int x, int y, int size, Tiles tile) {
+
+        for (int rows = x; rows < x + size; rows++) {
+            for (int cols = y; cols < y + size; cols++) {
                 setBlock(tile, rows, cols);
             }
         }
-        
+
     }
-    
+
 }
